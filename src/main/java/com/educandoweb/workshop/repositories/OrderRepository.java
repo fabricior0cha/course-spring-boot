1245d463d67d419051a73943949339b7ad4a97ca
unknown <fabriciorochapalmeira@gmail.com>
2022-02-25 18:11:58 -0300

    Category

--- src/main/java/com/educandoweb/workshop/repositories/OrderRepository.java
+++ src/main/java/com/educandoweb/workshop/repositories/OrderRepository.java
@@ -3,7 +3,6 @@ package com.educandoweb.workshop.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.workshop.entities.Order;
-import com.educandoweb.workshop.entities.User;
 
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
